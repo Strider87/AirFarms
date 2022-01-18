93c69962d6d7f3baf5cc4957c3085658332c9cd4
SimplifyOrg <abhishek.en@gmail.com>
2022-01-18 17:16:14 +0530

    Update urls.py

--- Backend/airfarms/mapfarm/urls.py
+++ Backend/airfarms/mapfarm/urls.py
@@ -1,10 +1,16 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
-app_name = 'mapfarm'
+#app_name = 'mapfarm'
+
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register(r'mapfarm/handle', views.FarmMapViewSet)
 
 urlpatterns = [
-	path('farm-map/<int:pk>', views.map, name="farm-map"),
-	path('save-shape/<int:pk>', views.savePolygon, name="save-shape"),
-	path('farm-dashboard/<int:pk>', views.showDashboard, name="farm-dashboard"),
+	# path('farm-map/<int:pk>', views.map, name="farm-map"),
+	# path('save-shape/<int:pk>', views.savePolygon, name="save-shape"),
+	# path('farm-dashboard/<int:pk>', views.showDashboard, name="farm-dashboard"),
+    path('', include(router.urls), name='handle-farm-map'),
 	]
