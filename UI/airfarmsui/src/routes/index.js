ddbe838405c92123a12d93f3b78f7dbba0f48ee1
SimplifyOrg <abhishek.en@gmail.com>
2022-02-16 14:21:30 +0530

    Add files via upload

--- UI/airfarmsui/src/routes/index.js
+++ UI/airfarmsui/src/routes/index.js
@@ -9,6 +9,11 @@ import RouteWrapper from './RouteWrapper';
 import UserDashboard from '../pages/UserDashboard';
 import UserProfilePage from '../pages/UserProfilePage'
 import CreateFarm from '../pages/CreateFarm'
+import Farm from '../pages/Farm'
+import ProjectPage from '../pages/ProjectPage'
+import ProjectList from '../pages/ProjectList'
+import DiscussionBoardPage from '../pages/DiscussionBoardPage';
+import TodoPage from '../pages/TodoPage'
 
 function Routes() {
     return (
@@ -21,6 +26,11 @@ function Routes() {
             <RouteWrapper path="/resetpassword" exact component={ResetPassword} />
             <RouteWrapper path="/user-dashboard" exact isPrivate component={UserDashboard} />
             <RouteWrapper path="/create-farm" exact isPrivate component={CreateFarm} />
+            <RouteWrapper path="/load-farm" exact isPrivate component={Farm} />
+            <RouteWrapper path="/load-project" exact isPrivate component={ProjectPage} />
+            <RouteWrapper path="/load-projectlist" exact isPrivate component={ProjectList} />
+            <RouteWrapper path="/discussion-board" exact isPrivate component={DiscussionBoardPage} />
+            <RouteWrapper path="/todo" exact isPrivate component={TodoPage} />
             <RouteWrapper component={Signin}/>
         </Switch>
         
