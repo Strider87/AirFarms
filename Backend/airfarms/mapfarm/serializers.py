72889902e4e55e3dc44f4d9e10b8410529ac50f3
SimplifyOrg <abhishek.en@gmail.com>
2022-02-16 14:05:58 +0530

    Add files via upload

--- Backend/airfarms/mapfarm/serializers.py
+++ Backend/airfarms/mapfarm/serializers.py
@@ -1,7 +1,14 @@
 from .models import FarmMap
-from rest_framework import serializers
+from rest_framework_gis import serializers
 
-class FarmMapSerializer(serializers.ModelSerializer):
+class FarmMapSerializer(serializers.GeoFeatureModelSerializer):
     class Meta:
         model = FarmMap
+        geo_field = "farmBoundary"
         fields = ('__all__')
+
+        def to_representation(self, instance):
+        #Convert `geom` to srid 4326."""
+            ret = super().to_representation(instance)
+            ret['farmBoundary'] = ret['farmBoundary'].transform(4326)
+            return ret  
--- Backend/airfarms/mapfarm/views.py
+++ Backend/airfarms/mapfarm/views.py
@@ -7,7 +7,7 @@ from farms.models import Farm
 from mapfarm.serializers import FarmMapSerializer
 from .models import FarmMap
 from .forms import NewFarmShape
-from rest_framework import generics, permissions, viewsets
+from rest_framework import permissions, viewsets
 import json
 from django.contrib.gis.geos import GEOSGeometry
 # import the logging library
@@ -23,12 +23,13 @@ class FarmMapViewSet(viewsets.ModelViewSet):
     #parser_classes = (MultiPartParser, FormParser)
     queryset = FarmMap.objects.all()
     serializer_class = FarmMapSerializer
-    #lookup_field = 'farm_id'
+    lookup_field = 'farm_id'
 
     def retrieve(self, request, *args, **kwargs):
-        farm_id = kwargs.get('farm', None)
-        farm_obj = Farm.objects.get(id=farm_id)
-        self.queryset = FarmMap.objects.filter(farm=farm_obj).distinct()
+        farm_id = kwargs.get('farm_id', None)
+        if farm_id is not None:
+            farm_obj = Farm.objects.get(id=farm_id)
+            self.queryset = FarmMap.objects.filter(farm=farm_obj).distinct()
         return super(FarmMapViewSet, self).retrieve(request, *args, **kwargs)
 
     def perform_create(self, serializer):
@@ -51,66 +52,66 @@ class FarmMapViewSet(viewsets.ModelViewSet):
             break
         serializer.save(farm=farm_obj, farmBoundary=farmBoundaryData)
 
-'''
-Basic view for displaying a map
-'''
-@login_required
-def map(request, pk):
-	farm = get_object_or_404(Farm, pk=pk)
+# '''
+# Basic view for displaying a map
+# '''
+# @login_required
+# def map(request, pk):
+# 	farm = get_object_or_404(Farm, pk=pk)
 
-	lat_a = request.GET.get("lat_a")
-	long_a = request.GET.get("long_a")
+# 	lat_a = request.GET.get("lat_a")
+# 	long_a = request.GET.get("long_a")
 
-	#if request.method == "POST":
-	#	form = NewFarmShape(request.POST)
-	#	if form.is_valid():
-	#		data = form.save(commit=False)
-	#		#data.farmBoundary = user
-	#		data.save()
-	#		messages.success(request, f'Posted Successfully')
-	#		return HttpResponseRedirect(reverse('mapfarm:farm-map', kwargs={'pk': data.id}))
-	#		#return redirect('mapfarm:farm-map', pk=[data.id])
-	#else:
-	#	form = NewFarmShape()
+# 	#if request.method == "POST":
+# 	#	form = NewFarmShape(request.POST)
+# 	#	if form.is_valid():
+# 	#		data = form.save(commit=False)
+# 	#		#data.farmBoundary = user
+# 	#		data.save()
+# 	#		messages.success(request, f'Posted Successfully')
+# 	#		return HttpResponseRedirect(reverse('mapfarm:farm-map', kwargs={'pk': data.id}))
+# 	#		#return redirect('mapfarm:farm-map', pk=[data.id])
+# 	#else:
+# 	#	form = NewFarmShape()
 
-	context = {
-	"google_api_key": settings.GOOGLE_MAP_API_KEY,
-	"lat_a": lat_a,
-	"long_a": long_a,
-	"origin": f'{lat_a}, {long_a}',
-	"farm": farm,
-	"pk": pk
-	#"form": form
-	}
+# 	context = {
+# 	"google_api_key": settings.GOOGLE_MAP_API_KEY,
+# 	"lat_a": lat_a,
+# 	"long_a": long_a,
+# 	"origin": f'{lat_a}, {long_a}',
+# 	"farm": farm,
+# 	"pk": pk
+# 	#"form": form
+# 	}
 
-	return render(request, 'home/map/map.html', context)
+# 	return render(request, 'home/map/map.html', context)
 
-@login_required
-def savePolygon(request, pk):
-	farm = get_object_or_404(Farm, pk=pk)
-	if request.method == 'POST':
-		farmMap = FarmMap.objects.create(farm=farm)
-		data = request.body.decode('utf-8')
-		jsonData = json.loads(data)
-		for feature in jsonData['features']:
-			try:
-				#Save Polygon Geometry to DB
-				farmMap.farmBoundary = GEOSGeometry(str(feature['geometry']))
-			except (TypeError, ValueError) as exc:
-				# If the geometry_str is not a valid WKT, EWKT or HEXEWKB string
-				# or is None then either continue, break or do something else.
-				print(exc)
-				break
-			break
-		farmMap.save()
-		lat_a = request.GET.get("lat_a")
-		long_a = request.GET.get("long_a")
+# @login_required
+# def savePolygon(request, pk):
+# 	farm = get_object_or_404(Farm, pk=pk)
+# 	if request.method == 'POST':
+# 		farmMap = FarmMap.objects.create(farm=farm)
+# 		data = request.body.decode('utf-8')
+# 		jsonData = json.loads(data)
+# 		for feature in jsonData['features']:
+# 			try:
+# 				#Save Polygon Geometry to DB
+# 				farmMap.farmBoundary = GEOSGeometry(str(feature['geometry']))
+# 			except (TypeError, ValueError) as exc:
+# 				# If the geometry_str is not a valid WKT, EWKT or HEXEWKB string
+# 				# or is None then either continue, break or do something else.
+# 				print(exc)
+# 				break
+# 			break
+# 		farmMap.save()
+# 		lat_a = request.GET.get("lat_a")
+# 		long_a = request.GET.get("long_a")
 
-		return HttpResponseRedirect(reverse('mapfarm:farm-dashboard', kwargs={'pk': pk}))
+# 		return HttpResponseRedirect(reverse('mapfarm:farm-dashboard', kwargs={'pk': pk}))
 
-@login_required
-def showDashboard(request, pk):
-	farm = get_object_or_404(Farm, pk=pk)
+# @login_required
+# def showDashboard(request, pk):
+# 	farm = get_object_or_404(Farm, pk=pk)
 
-	context = {"pk":pk}
-	return render(request, 'home/farms/farm_dashboard.html', context)
+# 	context = {"pk":pk}
+# 	return render(request, 'home/farms/farm_dashboard.html', context)
